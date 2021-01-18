1c34022bd0e56dbc7950c5b7f7fb3f3a754bc6ee
zakaria <zakariaselassi@gmail.com>
2021-01-18 11:48:32 +0100

    init + exercice 1.1 + 1.2

---
+++ exercice1_1.js
@@ -0,0 +1,18 @@
+const readlineSync = require("readline-sync");
+
+let userName = readlineSync.question('can you give me your name please?');
+console.log("Hello" + userName);
+
+let age = 25;
+
+age = 36;
+age = 18;
+
+console.log("age : " + age);
+
+let htmlBadges = new Number(readlineSync.question('How many HTML badge do you have ?'))
+
+let cssBadges = new Number(readlineSync.question('How many css badge do you have ?'))
+
+let total = htmlBadges + cssBadges;
+console.log('woaw , you have ' + total);
