80d8411a243f61ff8b9ab6d7e17b41a57a56c2b7
zakaria <zakariaselassi@gmail.com>
2021-01-19 14:32:56 +0100

    update

--- exercice4_3.js
+++ exercice4_3.js
@@ -1,11 +1,18 @@
+const readlineSync = require("readline-sync");
+
 function rand10(alea) {
     alea = Math.random() * (10 - 1) + 1;
 
     return Math.floor(alea);
 }
 
+function saisir() {
+    n = new Number(readlineSync.question("Entrer un nombre de nombre aleatoire a générer : "));
+    return n;
+}
+
 function multiRand(n) {
-    n = 5;
+
     let array = [];
     for (let i = 0; i < n; i++) {
 
@@ -15,4 +22,5 @@ function multiRand(n) {
     return array;
 }
 
-console.log(multiRand());
+console.log(saisir());
+console.log(multiRand(n));
