da7434d9ea31239faeed00fb2323bf9a524fd0f6
zakaria <zakariaselassi@gmail.com>
2021-01-19 10:58:14 +0100

    exercice4_1.js

---
+++ exercice4_1.js
@@ -0,0 +1,18 @@
+const readlineSync = require("readline-sync");
+
+function saisir() {
+    largeur = new Number(readlineSync.question("Saisir un nombre pour la largeur : "));
+    long = new Number(readlineSync.question("Saisire un nombre pour la long : "));
+    return largeur, long;
+}
+
+
+
+function calculSurface(largeur, long) {
+
+
+    return largeur * long;
+}
+
+console.log(saisir());
+console.log(calculSurface(largeur, long));
