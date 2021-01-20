b0ee24e0c78875ad18f3a05444e7f99ad3e54914
zakaria <zakariaselassi@gmail.com>
2021-01-20 20:59:36 +0100

    exercice6_2.js

--- exercice6_2.js
+++ exercice6_2.js
@@ -6,15 +6,38 @@ class Rectangle {
         this.length = length;
     }
     collides(otherRectangle) {
-        if (this.topLeftXPos === otherRectangle.topLeftXPos || this.topLeftYPos === otherRectangle.topLeftYPos || this.width === otherRectangle.width || this.length === otherRectangle.length) {
+        if (this.topLeftXPos === otherRectangle.topLeftXPos || this.topLeftYPos === otherRectangle.topLeftYPos || this.width === otherRectangle.length || this.length === otherRectangle.width) {
             return true;
         } else {
             return false;
         }
     }
+    initRec() {
+
+        for (let i = 0; i < 1000; i++) {
+
+            topLeftXPos = Math.random() * (10 - 1) + 1;
+            topLeftYPos = Math.random() * (10 - 1) + 1;
+            width = Math.random() * (10 - 1) + 1;
+            length = Math.random() * (10 - 1) + 1;
+        }
+        return topLeftXPos, topLeftYPos, width, length;
+    }
+
+    afficher() {
+        let array = [];
+        for (let i = 0; i < 1000; i++) {
+
+            array.push(topLeftXPos, topLeftYPos, width, length);
+
+
+        }
+        return array;
+    }
 }
 
-let rect1 = new Rectangle(2, 2, 3, 4);
-let rect2 = new Rectangle(5, 5, 5, 2);
-
-console.log(rect1.collides(rect2));
+let rect1 = new Rectangle();
+let topLeftXPos, topLeftYPos, width, length;
+rect1.initRec();
+console.log(rect1.initRec());
+console.log(rect1.afficher());
